bf1a4fb37ba5441b7aef217a5afaea7080c8bfc7
markgreene74 <markgreene74@users.noreply.github.com>
2023-01-02 23:16:49 +0000

    completed exercises/primitive_types

--- exercises/04_primitive_types/primitive_types5.rs
+++ exercises/04_primitive_types/primitive_types5.rs
@@ -5,11 +5,10 @@
 // Execute `rustlings hint primitive_types5` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
+    // see https://doc.rust-lang.org/rust-by-example/primitives/tuples.html
     let cat = ("Furry McFurson", 3.5);
-    let /* your pattern here */ = cat;
+    let (name, age) = cat;
 
     println!("{} is {} years old.", name, age);
 }
