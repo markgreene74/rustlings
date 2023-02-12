027aed2ce1e883143221b7906abe50e9ec91537b
markgreene74 <markgreene74@users.noreply.github.com>
2023-02-12 21:48:58 +0000

    updated exercises/intro solution

--- exercises/00_intro/intro1.rs
+++ exercises/00_intro/intro1.rs
@@ -35,6 +35,6 @@ fn main() {
     println!(
         "If you want to use rust-analyzer, Rust's LSP implementation, make sure your editor is set"
     );
-    println!("up, and then run `rustlings lsp` before continuing.")
+    println!("up, and then run `rustlings lsp` before continuing.");
     println!("1 Jan 2023")
 }
