b415d4f587b4e1e6f4f109fb471284c7da5639f1
markgreene74 <markgreene74@users.noreply.github.com>
2023-03-25 13:46:19 +0000

    completed exercises/clippy

--- exercises/22_clippy/clippy3.rs
+++ exercises/22_clippy/clippy3.rs
@@ -3,28 +3,31 @@
 // Here's a couple more easy Clippy fixes, so you can see its utility.
 // No hints.
 
-// I AM NOT DONE
-
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    if my_option.is_none() {
+    if let Some(value) = my_option {
+        // we could use Some(_) here, as we really don't have any further
+        // use for `value`, but that causes another clippy warning ...
         my_option.unwrap();
     }
 
     let my_arr = &[
-        -1, -2, -3
-        -4, -5, -6
+        -1, -2, -3,
+        -4, -5, -6,
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
-    let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
-    println!("This Vec is empty, see? {:?}", my_empty_vec);
+    let mut my_empty_vec = vec![1, 2, 3, 4, 5];
+    println!("This Vec is empty, see? {:?}", my_empty_vec.clear());
 
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    value_a = value_b;
-    value_b = value_a;
+    //replace
+    // value_a = value_b;
+    // value_b = value_a;
+    // with
+    std::mem::swap(&mut value_a, &mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
